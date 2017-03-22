8114f56100a6c28c483502e4a1833551b1e04f93
Minh Nguyen <thienphong215@gmail.com>
2017-03-23 05:26:05 +0700

    Matches time

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Moment from 'react-moment'
 import {Link} from 'react-router';
 import {Card} from 'material-ui/Card';
 
@@ -8,20 +9,33 @@ class TableRow extends Component {
         return (
             <Card className="TableRow row u-position--relative">
                 <div className="TableRow-head row">
-                    <div className="col-md-8 text-center">
+                    <div className="col-md-8">
                         {match.tournament} - {match.mode_name}
+                    </div>
+                    <div className="col-md-4 text-right">
+                        {
+                            match.status === "Live"
+                                ?
+                                    <span>Live</span>
+                                :
+                                    <div className="TableRow-time">
+                                        <Moment fromNow>{match.time}</Moment>
+                                    </div>
+                        }
                     </div>
                 </div>
                 <div className="TableRow-body container">
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-2">
                             <div className="TableRow-body-name">
                                 <Link to={"/team/"+match.teama}>
                                     {match.teama}
                                 </Link>
                                 <div>{match.ratioa}</div>
                             </div>
-                            <div className="TableRow-body-image">
+                        </div>
+                        <div className="col-md-2">
+                            <div className="TableRow-body-image text-center">
                                 <Link to={"/team/"+match.teama}>
                                     <img src={match.logo_a} alt={match.teama}/>
                                 </Link>
@@ -32,23 +46,25 @@ class TableRow extends Component {
                                 match.status === "Settled"
                                     ?
                                         <div className="TableRow-body-score">
-                                            <span>{match.scorea}</span><span>{match.scoreb}</span>
+                                            <span>{match.scorea} </span><span> {match.scoreb}</span>
                                         </div>
                                     : ""
                             }
                             {match.bestof}
                         </div>
-                        <div className="col-md-3">
-                            <div className="TableRow-body-name">
+                        <div className="col-md-2">
+                            <div className="TableRow-body-image text-center">
+                                <Link to={"/team/"+match.teamb}>
+                                    <img src={match.logo_b} alt={match.teamb}/>
+                                </Link>
+                            </div>
+                        </div>
+                        <div className="col-md-2">
+                            <div className="TableRow-body-name text-right">
                                 <Link to={"/team/"+match.teamb}>
                                     {match.teamb}
                                 </Link>
                                 <div>{match.ratiob}</div>
-                            </div>
-                            <div className="TableRow-body-image">
-                                <Link to={"/team/"+match.teamb}>
-                                    <img src={match.logo_b} alt={match.teamb}/>
-                                </Link>
                             </div>
                         </div>
                     </div>
@@ -57,7 +73,7 @@ class TableRow extends Component {
                             ? 	<div>
                                 <div className="row">
                                     <div className="TableRow-body-result">
-                                        <div className="col-md-8 text-center">{match.winner} won</div>
+                                        <div className="col-md-10 text-center">{match.winner} won</div>
                                     </div>
                                 </div>
                             </div>
