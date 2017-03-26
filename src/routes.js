2587907ea943c641dcf38e90abecd7ec3a3dca95
Minh Nguyen <thienphong215@gmail.com>
2017-03-26 16:17:51 +0700

    CSGO route

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import App from './containers/App'
 import Dota from './containers/Dota'
+import Csgo from './containers/Csgo'
 import MatchDetail from './containers/MatchDetail'
 import TeamDetail from './containers/TeamDetail'
 
 export default <Route path="/">
     <IndexRoute component={App}/>
     <Route path="/dota" component={Dota} />
+    <Route path="/csgo" component={Csgo} />
     <Route path="/match/:id" component={MatchDetail} />
     <Route path="/team/:name" component={TeamDetail}/>
 </Route>
