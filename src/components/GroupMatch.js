11765e42fb0b6397b173477a93a3e50e92974cfa
Minh Nguyen <thienphong215@gmail.com>
2017-04-02 22:53:19 +0700

    Match winner position

--- src/components/GroupMatch.js
+++ src/components/GroupMatch.js
@@ -5,10 +5,19 @@ class GroupMatch extends Component {
 
     renderTableRow() {
         const { groupMatch } = this.props;
+        var t = {};
+        for (let match in groupMatch.matches) {
+            if (groupMatch.matches[match].mode_name === "Match Winner") {
+                t = groupMatch.matches[0];
+                groupMatch.matches[0] = groupMatch.matches[match];
+                groupMatch.matches[match] = t;
+            }
+        }
         return groupMatch.matches.map((i) => <TableRow match={i} key={i.id.toString()}/>);
     }
 
     render () {
+        console.log(this.props);
         const tableRow = this.renderTableRow();
         return (
             <div className="GroupMatch">
