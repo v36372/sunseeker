09f2148328d5f0918f7c71a3670d9ed06d4fdeff
Minh Nguyen <thienphong215@gmail.com>
2017-04-29 07:39:45 +0700

    Rework match page

--- src/containers/MatchDetail.js
+++ src/containers/MatchDetail.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Moment from 'react-moment'
 import {Link} from 'react-router';
-import {Card} from 'material-ui/Card';
 import NavigationBar from '../components/NavigationBar'
 import { fetchMatch } from '../actions'
 
@@ -16,87 +15,72 @@ class MatchDetail extends Component {
         const { match } = this.props;
 
         return (
-            <div>
+            <div className="MatchDetail">
                 <NavigationBar />
-            <div className="container">
-            <Card className="TableRow row u-position--relative">
-                <div className="TableRow-head row">
-                    <div className="col-md-8">
-                        {match.tournament} - {match.mode_name}
-                    </div>
-                    <div className="col-md-4 text-right">
-                        {
-                            match.status === "Live"
-                                ?
-                                <span>Live</span>
-                                :
-                                <div className="TableRow-time">
-                                    <Moment fromNow>{match.time}</Moment>
+                <div className="container">
+                    <div className="Match col-md-12">
+
+                        <div className="Match-title row">
+                            <div className="col-md-6">
+                                {match.tournament} - {match.mode_name}
+                            </div>
+                            <div className="col-md-6 text-right">
+                                {
+                                    match.status === "Live"
+                                        ?
+                                        <span>Live</span>
+                                        :
+                                        <div className="TableRow-time">
+                                            <Moment fromNow>{match.time}</Moment>
+                                        </div>
+                                }
+                            </div>
+                        </div>
+
+                        <hr/>
+
+                        <div className="row">
+                            <div className="col-md-4">
+                                <div className="row text-center">
+                                    <Link to={`/team/${match.teama}?game=${match.game}`}>
+                                        <img className="Match-team-logo" src={match.logo_a} alt={match.teama}/>
+                                    </Link>
                                 </div>
-                        }
-                    </div>
-                </div>
-                <div className="TableRow-body container">
-                    <div className="row">
-                        <div className="col-md-2">
-                            <div className="TableRow-body-name">
-                                <Link to={`/team/${match.teama}?game=${match.game}`}>
-                                    {match.teama}
-                                </Link>
-                                <div>{match.ratioa}</div>
+                                <div className="row text-center">
+                                    <Link to={`/team/${match.teama}?game=${match.game}`} className="Match-team-title">
+                                        {match.teama}
+                                    </Link>
+                                </div>
+                                <div className="Match-ratio row text-center">
+                                    {match.ratioa}
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div className="TableRow-body-image text-center">
-                                <Link to={`/team/${match.teama}?game=${match.game}`}>
-                                    <img src={match.logo_a} alt={match.teama}/>
-                                </Link>
+                            <div className="col-md-4 text-center">
+                                <div className="Match-score">
+                                    { match.status === "Settled" ? match.scorea + ' - ' + match.scoreb : "" }
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-2 text-center">
-                            {
-                                match.status === "Settled"
-                                    ?
-                                    <div className="TableRow-body-score">
-                                        <span>{match.scorea} </span><span> {match.scoreb}</span>
-                                    </div>
-                                    : ""
-                            }
-                            {match.bestof}
-                        </div>
-                        <div className="col-md-2">
-                            <div className="TableRow-body-image text-center">
-                                <Link to={`/team/${match.teamb}?game=${match.game}`}>
-                                    <img src={match.logo_b} alt={match.teamb}/>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="col-md-2">
-                            <div className="TableRow-body-name text-right">
-                                <Link to={`/team/${match.teamb}?game=${match.game}`}>
-                                    {match.teamb}
-                                </Link>
-                                <div>{match.ratiob}</div>
-                            </div>
-                        </div>
-                    </div>
-                    {
-                        match.status === "Settled"
-                            ? 	<div>
-                            <div className="row">
-                                <div className="TableRow-body-result">
-                                    <div className="col-md-10 text-center">{match.winner} won</div>
+                            <div className="col-md-4">
+                                <div className="row text-center">
+                                    <Link to={`/team/${match.teamb}?game=${match.game}`}>
+                                        <img className="Match-team-logo" src={match.logo_b} alt={match.teamb}/>
+                                    </Link>
+                                </div>
+                                <div className="row text-center">
+                                    <Link to={`/team/${match.teamb}?game=${match.game}`} className="Match-team-title">
+                                        {match.teamb}
+                                    </Link>
+                                </div>
+                                <div className="Match-ratio row text-center">
+                                    {match.ratiob}
                                 </div>
                             </div>
                         </div>
-                            :	''
-                    }
+
+                        <hr/>
+
+                    </div>
                 </div>
-                <div className="TableRow-background">
-                    <img src={match.tournament_logo} alt={match.tournament} />
-                </div>
-            </Card>
-            </div>
             </div>
         )
     }
