5e9905fed0098b965cb0ad265ec26cb4c4e56224
Minh Nguyen <thienphong215@gmail.com>
2017-05-14 01:37:58 +0700

    Add serialize helper

---
+++ src/helper/serialize.js
@@ -0,0 +1,10 @@
+const serialize = function(obj) {
+    let str = [];
+    for(let p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
+}
+
+export default serialize
